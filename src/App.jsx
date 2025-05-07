2949630588e0cf5261f32e40a5f40657a9706a09
DhruvReddyS <dhruvreddy07@gmail.com>
2025-05-07 21:53:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import Signinup from './pages/signin-up/signin-up';
+import About from './pages/about/about';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Signinup />} />
+      <Route path="/signup" element={<Signinup />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
